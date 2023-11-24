a6532e45513af37b079f3a81590b3dd2d4a411bc
SachinUthpala <gunasekara.s.u.d.a@gmail.com>
2023-11-24 09:04:50 +0530

    updates

--- src/componets/header/Header.jsx
+++ src/componets/header/Header.jsx
@@ -1,28 +1,35 @@
 import React from 'react'
 import css from './Header.module.scss'
 import {BiPhoneCall} from 'react-icons/bi'
+import {motion} from 'framer-motion'
+import { headerVariants } from '../../utils/motion'
 
 const Header = () => {
   return (
-    <div className={`paddings ${css.wrapper}`}>
+    <motion.div 
+    initial = "hidden"
+    whileInView="show"
+    variants={headerVariants}
+    viewport={{once:true , amount:0.25}}
+    className={`paddings ${css.wrapper}`}>
         <div className={`flexCenter innerWidth ${css.container}`}>
             <div className={css.name}>
                 Sachin
             </div>
-            <ul>
+            <ul className={`flexCenter ${css.menu}`}>
                 <li><a href="">Services</a></li>
                 <li><a href="">Experience</a></li>
                 <li><a href="">Portfolio</a></li>
                 <li><a href="">Testimonials</a></li>
-                <li>
+                <li className={`flexCenter ${css.phone}`}>
                     <p>
-                        +9476 2712 200
+                        0762712200
                     </p>
                     <BiPhoneCall size={"40px"}/>
                 </li>
             </ul>
         </div>
-    </div>
+    </motion.div>
   )
 }
 
